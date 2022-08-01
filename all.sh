db3f2d32d5459c0de59fbb104fa07d36dcbd274f
Romain Pelisse <belaran@gmail.com>
2022-08-01 16:07:59 +0200

    Misc: enhance all.sh

--- all.sh
+++ all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -eo pipefail
-rm -rf downstream
-mkdir downstream
+rm -rf downstream upstream
+mkdir -p downstream upstream
 for play in $(ls -1 playbooks/*.yml)
 do
   ansible-playbook "${play}" &
